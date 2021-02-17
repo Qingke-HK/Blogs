ca980985ba694841113737e22014cbb80eea3308
Kai.He <kai.he.0811@qq.com>
2021-02-17 11:01:10 +0800

    测试自动化部署

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,10 @@
 
 echo "start build step"
 
-jekyll new ../../temp-build-dir
+jekyll new ../temp-build-dir
 
-cp -r * ../../temp-build-dir/_posts
+cp -r * ../temp-build-dir/_posts
 
-ls ../../temp-build-dir/_posts
+ls ../temp-build-dir/_posts
 
 echo "end build step"
